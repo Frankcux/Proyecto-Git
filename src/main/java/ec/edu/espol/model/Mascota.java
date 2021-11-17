4dd0b5d41c608485aedb1c083c0acea8cf81861f
Angel Loaiza <anlosanc@espol.edu.ec>
2021-11-16 23:28:19 -0500

    Agregue mas atributos en la clase Mascota

--- src/main/java/ec/edu/espol/model/Mascota.java
+++ src/main/java/ec/edu/espol/model/Mascota.java
@@ -1,8 +1,15 @@
 
 package ec.edu.espol.model;
 
+import java.util.Date;
+
 
 public class Mascota {
+    private int id;
     private String nombre;
+    private String raza;
+    private String tipo;
+    private Date fechaNacimiento;
+    private int idDueno;
     
 }
