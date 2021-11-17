a482500be73dde83660873cd88288b8313440ed3
Angel Loaiza <anlosanc@espol.edu.ec>
2021-11-17 14:11:19 -0500

    Agregue constructores, getters, setters y comportamientos en las clases Concurso y Premios

--- src/main/java/ec/edu/espol/model/Concurso.java
+++ src/main/java/ec/edu/espol/model/Concurso.java
@@ -1,10 +1,192 @@
 
 package ec.edu.espol.model;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
 
 
 public class Concurso {
+    //DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy"); 
+    //LocalDate fecha = LocalDate.parse("02/10/2020", formato); 
+    private int id;
     private String nombre;
-    private Date fecha;
+    private LocalDate fecha;
+    private LocalDate fechaInscripcion;
+    private LocalDate fechaCierreInscripcion;
+    private String tematica;
+    private double costo;
+    private ArrayList<Inscripcion> inscripciones;
+    private ArrayList<Premios> premios;
+    private ArrayList<Criterio> criterio;
+    
+    
+    public Concurso(int id, String nombre, LocalDate fecha, LocalDate fechaInscripcion, LocalDate fechaCierreInscripcion, String tematica, double costo){
+        this.id = id;
+        this.nombre = nombre; 
+        this.fecha = fecha;
+        this.fechaCierreInscripcion = fechaCierreInscripcion;
+        this.fechaInscripcion = fechaInscripcion;
+        this.tematica = tematica;
+        this.costo = costo;
+        
+    
+    }
+    // GETTERS
+    public int getId() {
+        return id;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public LocalDate getFecha() {
+        return fecha;
+    }
+    public LocalDate getFechaInscripcion() {
+        return fechaInscripcion;
+    }
+    public LocalDate getFechaCierreInscripcion() {
+        return fechaCierreInscripcion;
+    }
+     public String getTematica() {
+        return tematica;
+    }
+      public double getCosto() {
+        return costo;
+    }
+      public ArrayList<Inscripcion> getInscripciones() {
+        return inscripciones;
+    }
+      public ArrayList<Premios> getPremios() {
+        return premios;
+    }
+      public ArrayList<Criterio> getCriterio() {
+        return criterio;
+    }
+
+    //SETTERS
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+    public void setFechaInscripcion(LocalDate fechaInscripcion) {
+        this.fechaInscripcion = fechaInscripcion;
+    }
+    public void setFechaCierreInscripcion(LocalDate fechaCierreInscripcion) {
+        this.fechaCierreInscripcion = fechaCierreInscripcion;
+    }
+    public void setTematica(String tematica) {
+        this.tematica = tematica;
+    }
+    public void setCosto(double costo) {
+        this.costo = costo;
+    }
+    public void setInscripciones(ArrayList<Inscripcion> inscripciones) {
+        this.inscripciones = inscripciones;
+    }
+    public void setPremios(ArrayList<Premios> premios) {
+        this.premios = premios;
+    }
+    public void setCriterio(ArrayList<Criterio> criterio) {
+        this.criterio = criterio;
+    }
+    
+    @Override
+    
+    public String toString(){
+        return "Concurso{ Id: "+ id + ",nombre: "+ nombre+", fecha: "+ fecha+", fecha de inscripcion: "+ fechaInscripcion+ ", fecha de cierre de inscripcion: "+ fechaCierreInscripcion+ ", temática: "+ tematica+", costo de inscripcion: "+ costo+" }";
+        
+    }
+    @Override
+    
+    public boolean equals(Object o) {
+        if( o != null && o instanceof Concurso){
+            Concurso c2 = (Concurso)o;
+            return (this.id == c2.id) ;
+        }return false;
+    }
+     public void  saveFile(String nomfile){
+        try(PrintWriter pw = new PrintWriter(new FileOutputStream(new File(nomfile),true)))
+        {
+            pw.println(this.id + "|"+ this.nombre+ "|" + this.fecha + "|"+ this.fechaInscripcion+ "|"+ this.fechaCierreInscripcion+ "|"+ this.tematica+ "|"+ this.costo);
+        } catch(Exception e) {
+            System.out.println(e.getMessage());
+            
+        }
+    }
+    public static void saveFile(ArrayList<Concurso> concursos, String nomfile){
+        try(PrintWriter pw = new PrintWriter(new FileOutputStream(new File(nomfile))))
+        {
+           for( Concurso c: concursos)
+               pw.println(c.id + "|"+ c.nombre+ "|" + c.fecha + "|"+ c.fechaInscripcion + "|"+ c.fechaCierreInscripcion+ "|"+ c.tematica+ "|"+ c.costo); 
+        } catch(Exception e) {
+            System.out.println(e.getMessage());
+            
+        }
+    }
+    
+    public static ArrayList<Concurso> readFromFile(String nomfile){
+        ArrayList<Concurso> concursos = new ArrayList<>();
+        try (Scanner sc = new Scanner(new File(nomfile)))
+        {
+            while(sc.hasNextLine())
+            {
+                // linea = id|nombre|fecha|fechaInscripcion|fechaCierreInscripcion|tematica|costo
+                String linea = sc.nextLine();
+                String[] tokens = linea.split("\\|");
+                // int id,String nombre,LocalDate fecha,LocalDate fechaInscripcion,LocalDate fechaCierreInscripcion,String tematica, double costo
+                Concurso c = new Concurso(Integer.parseInt(tokens[0]),tokens[1],LocalDate.parse(tokens[2]),LocalDate.parse(tokens[3]),LocalDate.parse(tokens[4]), tokens[5], Double.parseDouble(tokens[6])  ) ;
+                concursos.add(c);
+            }
+            
+        }catch(Exception e) {
+            System.out.println(e.getMessage());
+            
+        }
+        return concursos;
+    
+    }
+    public static Concurso nextVector(Scanner sc){
+        
+        int id;
+        String tematica, nombre;
+        double costo;
+        LocalDate fecha, fechaIns, fechaCierreIns;
+        sc.useDelimiter("\n");
+       
+        sc.useLocale(Locale.US);
+        System.out.println("Ingrese el id del concurso: ");
+        id = sc.nextInt();
+        System.out.println("Ingrese el nombre de ese concurso: ");
+        nombre = sc.next();
+        System.out.println("Ingrese la fecha del concurso: ");
+        fecha = LocalDate.parse(sc.next());
+        System.out.println("Ingrese la fecha de inscripcion: ");
+        fechaIns = LocalDate.parse(sc.next());
+        System.out.println("Ingrese la fecha de cierre de inscripciones: ");
+        fechaCierreIns = LocalDate.parse(sc.next());
+        System.out.println("Ingrese la temática del concurso: ");
+        tematica = sc.next();
+        System.out.println("Ingrese el costo de la inscripcion: ");
+        costo = sc.nextDouble();
+        Concurso concurso = new Concurso(id,nombre,fecha,fechaIns,fechaCierreIns,tematica,costo);
+        return concurso;
+        
+    }
+    
+    
+    
+           
 }
--- src/main/java/ec/edu/espol/model/Premios.java
+++ src/main/java/ec/edu/espol/model/Premios.java
@@ -1,6 +1,142 @@
 
 package ec.edu.espol.model;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Premios {
+    private int id;
+    private int lugar;
+    private String descripcion;
+    private int idConcurso;
+    private Concurso concurso;
+    
+    public Premios(int id, int lugar, String des, int idConc){
+        this.id = id;
+        this.lugar= lugar;
+        this.descripcion = des;
+        this.idConcurso = idConc;
+    }
+ 
+    // Getters
+    
+    public int getId() {
+        return id;
+    }
+    public int getLugar() {
+        return lugar;
+    }
+    public String getDescripcion() {
+        return descripcion;
+    }
+    public int getIdConcurso() {
+        return idConcurso;
+    }
+    public Concurso getConcurso() {
+        return concurso;
+    }
+    //Setters
+    
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void setLugar(int lugar) {
+        this.lugar = lugar;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public void setIdConcurso(int idConcurso) {
+        this.idConcurso = idConcurso;
+    }
+
+    public void setConcurso(Concurso concurso) {
+        this.concurso = concurso;
+    }
+
+    @Override
+    
+    public String toString() {
+        return "Premios{" + "id=" + id + ", lugar=" + lugar + ", descripcion=" + descripcion + ", idConcurso=" + idConcurso + ", concurso=" + concurso + '}';
+    }
+    @Override
+    
+    public boolean equals(Object o) {
+        if( o != null && o instanceof Premios){
+            Premios p2 = (Premios)o;
+            return (this.descripcion.equals(p2.descripcion)) ;
+        }return false;
+    }
+    
+    public void  saveFile(String nomfile){
+        try(PrintWriter pw = new PrintWriter(new FileOutputStream(new File(nomfile),true)))
+        {
+            pw.println(this.id + "|"+ this.lugar+ "|" + this.descripcion + "|"+ this.idConcurso);
+        } catch(Exception e) {
+            System.out.println(e.getMessage());
+            
+        }
+    }
+    public static void saveFile(ArrayList<Premios> premios, String nomfile){
+        try(PrintWriter pw = new PrintWriter(new FileOutputStream(new File(nomfile))))
+        {
+           for( Premios p: premios)
+               pw.println(p.id + "|"+ p.lugar+ "|" + p.descripcion + "|"+ p.idConcurso); 
+        } catch(Exception e) {
+            System.out.println(e.getMessage());
+            
+        }
+    }
+    
+    public static ArrayList<Premios> readFromFile(String nomfile){
+        ArrayList<Premios> premios = new ArrayList<>();
+        try (Scanner sc = new Scanner(new File(nomfile)))
+        {
+            while(sc.hasNextLine())
+            {
+                // linea = id|lugar|descripcion|idConcurso
+                String linea = sc.nextLine();
+                String[] tokens = linea.split("\\|");
+                // int id,int lugar,String descripcion,int idconcurso
+                Premios p = new Premios(Integer.parseInt(tokens[0]),Integer.parseInt(tokens[1]),tokens[2],Integer.parseInt(tokens[3]));
+                premios.add(p);
+            }
+            
+        }catch(Exception e) {
+            System.out.println(e.getMessage());
+            
+        }
+        return premios;
+    
+    }
+    public static Premios nextVector(Scanner sc){
+        
+        int id, lugar,idConcurso ;
+        String descrip;
+        sc.useDelimiter("\n");
+        sc.useLocale(Locale.US);
+        System.out.println("Ingrese el id del premio: ");
+        id = sc.nextInt();
+        System.out.println("Ingrese el lugar de ese premio: ");
+        lugar = sc.nextInt();
+        System.out.println("Ingrese la descripción del premio: ");
+        descrip = sc.next();
+        System.out.println("Ingrese el id del Concurso al que pertenece ese premio");
+        idConcurso = sc.nextInt();
+        
+        Premios premio = new Premios(id,lugar,descrip,idConcurso);
+        return premio;
+        
+    }
+    
+    
+    
+    
     
 }
