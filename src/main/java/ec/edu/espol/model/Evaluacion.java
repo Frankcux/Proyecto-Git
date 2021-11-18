42130e3e7de5c9fe35f6fb909c221783cfedc463
Adair Abrigo <aabrigo@espol.edu.ec>
2021-11-18 15:01:27 -0500

    Agregue atributos a evaluacion e inscripcion

--- src/main/java/ec/edu/espol/model/Evaluacion.java
+++ src/main/java/ec/edu/espol/model/Evaluacion.java
@@ -2,5 +2,10 @@
 package ec.edu.espol.model;
 
 public class Evaluacion {
-    
+    private int id;
+    private double nota;
 }
+
+
+
+
--- src/main/java/ec/edu/espol/model/Inscripcion.java
+++ src/main/java/ec/edu/espol/model/Inscripcion.java
@@ -1,6 +1,34 @@
 
 package ec.edu.espol.model;
 
+import java.util.ArrayList;
+
 public class Inscripcion {
+    private int id;
+    private String fecha_inscripcion;
+    private double valor;
+
     
+    public Inscripcion(int id, String fecha_inscripcion, double valor) {
+        this.id = id;
+        this.fecha_inscripcion = fecha_inscripcion;
+        this.valor = valor;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public String getFecha_inscripcion() {
+        return this.fecha_inscripcion;
+    }
+
+    public double getValor() {
+        return this.valor;
+    }
+    
+    
+
+
+
 }
